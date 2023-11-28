f598bdc741dee1df45cb807bfcf016134e55a0ba
Arpit <arpitjaiswal9526@gmail.com>
2023-11-28 18:20:30 +0530

    Some important chabnges

--- fll.py
+++ fll.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import json
+from openai import OpenAI
 
 
 app = Flask(__name__)
@@ -9,24 +10,44 @@ app = Flask(__name__)
 def hello_world():
     if request.method == "POST":
         text = request.form['search']
-        url = "https://stablediffusionapi.com/api/v3/text2img/"
+        url = "https://stablediffusionapi.com/api/v4/dreambooth/"
+
+        client = OpenAI(organization="you_stable_diffusion_org_id_here", api_key="you_stable_diffusion_api_key_here")
+
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are creating a single word of full sentences"},
+            {"role": "user", "content": text}
+        ]
+        )
+
+        print(completion.choices[0].message.content)
+        text = completion.choices[0].message.content
 
         payload = json.dumps({
-        "key": "your_api_key",
-        "prompt": f"{text}",
-        "negative_prompt": None,
+        "key": "you_stable_diffusion_api_key_here",
+        "model_id": "midjourney",
+        "prompt": text,
         "width": "512",
         "height": "512",
         "samples": "1",
-        "num_inference_steps": "20",
+        "num_inference_steps": "30",
+        "safety_checker": "no",
+        "enhance_prompt": "yes",
         "seed": None,
         "guidance_scale": 7.5,
-        "safety_checker": "yes",
         "multi_lingual": "no",
         "panorama": "no",
         "self_attention": "no",
         "upscale": "no",
         "embeddings_model": None,
+        "lora_model": None,
+        "tomesd": "yes",
+        "use_karras_sigmas": "yes",
+        "vae": None,
+        "lora_strength": None,
+        "scheduler": "UniPCMultistepScheduler",
         "webhook": None,
         "track_id": None
         })
